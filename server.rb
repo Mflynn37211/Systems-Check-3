d7d38e5d27ebb4862f2c9e00bfcee9fe6919717f
Michael Flynn <mflynn37211@yahoo.com>
2014-08-30 21:54:13 -0400

    added the list of reciepes and the id page with the instructuins and descriptions of each reciepe.

--- server.rb
+++ server.rb
@@ -3,7 +3,9 @@ require 'pry'
 require 'sinatra'
 require 'sinatra/reloader'
 
-
+###################################################
+#                         Methods
+###################################################
 def db_connection
   begin
     connection = PG.connect(dbname: 'recipes')
@@ -16,12 +18,28 @@ def db_connection
 end
 
 
-  get '/recipes' do
-    query = "SELECT recipes.name FROM recipes ORDER BY name ASC"
-    db_connection do |conn|
-    @recipes_list = conn.exec(query)
-    end
+###################################################
+#                    DATA
+###################################################
 
-    erb :'recipe'
+  get '/recipes' do
+    query = "SELECT * FROM recipes
+              ORDER BY recipes.name ASC;"
+    recipes = db_connection do |conn|
+        conn.exec(query)
+    end
+    @recipes_list = recipes.to_a
+    erb :'recipe_list'
   end
 
+
+
+ get '/recipes/:id' do
+    query = "SELECT recipes.name AS name, recipes.instructions AS instructions, recipes.description AS description, ingredients.name AS ingredients, ingredients.recipe_id FROM recipes
+            JOIN ingredients ON ingredients.recipe_id = recipes.id WHERE ingredients.recipe_id = '#{params[:id]}';"
+    recipes = db_connection do |conn|
+    conn.exec(query)
+  end
+  @recipe_information = recipes.to_a
+  erb :'recipe_info'
+end
