82456b4f0136da14a176255676a9caf46e26a113
Helen Hood <helen.c.hood@gmail.com>
2014-09-02 11:12:28 -0400

    Whitespace & indentation edits

--- server.rb
+++ server.rb
@@ -6,6 +6,7 @@ require 'sinatra/reloader'
 ###################################################
 #                         Methods
 ###################################################
+
 def db_connection
   begin
     connection = PG.connect(dbname: 'recipes')
@@ -17,29 +18,32 @@ def db_connection
   end
 end
 
-
 ###################################################
 #                    DATA
 ###################################################
 
-  get '/recipes' do
-    query = "SELECT * FROM recipes
-              ORDER BY recipes.name ASC;"
-    recipes = db_connection do |conn|
-        conn.exec(query)
-    end
-    @recipes_list = recipes.to_a
-    erb :'recipe_list'
-  end
+get '/recipes' do
+  query = "SELECT * FROM recipes
+            ORDER BY recipes.name ASC;"
 
-
-
- get '/recipes/:id' do
-    query = "SELECT recipes.name AS name, recipes.instructions AS instructions, recipes.description AS description, ingredients.name AS ingredients, ingredients.recipe_id FROM recipes
-            JOIN ingredients ON ingredients.recipe_id = recipes.id WHERE ingredients.recipe_id = '#{params[:id]}';"
-    recipes = db_connection do |conn|
+  recipes = db_connection do |conn|
     conn.exec(query)
   end
+
+  @recipes_list = recipes.to_a
+
+  erb :'recipe_list'
+end
+
+get '/recipes/:id' do
+  query = "SELECT recipes.name AS name, recipes.instructions AS instructions, recipes.description AS description, ingredients.name AS ingredients, ingredients.recipe_id FROM recipes
+    JOIN ingredients ON ingredients.recipe_id = recipes.id WHERE ingredients.recipe_id = '#{params[:id]}';"
+
+  recipes = db_connection do |conn|
+    conn.exec(query)
+  end
+
   @recipe_information = recipes.to_a
+
   erb :'recipe_info'
 end
